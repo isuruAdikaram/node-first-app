2900ace10f0de4d74946fb4e19e60f59fb249bdf
Isuru Adikaram <izuru775@gmail.com>
2021-08-20 19:48:03 +1000

    minor changers

--- app.js
+++ app.js
@@ -2,12 +2,9 @@
 
 const http = require('http')
 
-const fs = require('fs')
+const routes = require('./routes')
 
-const server = http.createServer((req,res) => {
-    const url = req.url
-    const method = req.method
-    
-})
+
+const server = http.createServer(routes.handler)
 
 server.listen(3000)
