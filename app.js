9a2a95b145fb5c40b11d96744a38e911e98713b0
Isuru Adikaram <izuru775@gmail.com>
2021-08-22 09:56:34 +1000

    path filtering added

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 // server created
 
 const express = require('express')
-
 const bodyParser =require('body-parser')
 
 const adminRoutes = require('./routes/admin')
-
 const shopRoutes = require('./routes/shop')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:true}))
 
-app.use(adminRoutes)
-
+app.use('/admin',adminRoutes)
 app.use(shopRoutes)
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000)
