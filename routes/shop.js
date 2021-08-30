8d310e37121e759c675050b87c6fc185a24e7d0a
Isuru Adikaram <izuru775@gmail.com>
2021-08-30 21:31:25 +1000

    shop.pug added and shop.js modified

--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
 const path = require('path')
-const rootDir = require('../util/path')
+
 const express = require('express')
 
 const router = express.Router()
 
+const rootDir = require('../util/path')
 const adminData = require('./admin')
 
 router.get('/',(req,res,next)=>{
-    console.log('shop.js',adminData.products)   
-    res.sendFile(path.join(rootDir,'views','shop.html')) 
+    const products = adminData.products
+    res.render('shop',{ prods:products, docTitle:'shop' })
 })
 
 module.exports = router
