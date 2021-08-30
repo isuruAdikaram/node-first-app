487d1906d26fe9db3424904c64ded431b12245a6
Isuru Adikaram <izuru775@gmail.com>
2021-08-30 23:38:28 +1000

    res.status(404).render('404') added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 const app = express()
-
+// this is for pug(start with this even in other products if working with pug)
 app.set('view engine','pug')
 app.set('views','views')
 
@@ -18,7 +18,8 @@ app.use('/admin',adminData.routes)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    res.status(404).render('404')
+    // res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 app.listen(3000)
